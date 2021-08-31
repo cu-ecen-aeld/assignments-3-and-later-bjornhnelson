bb938e8dfbbec737777d1e8c11823d4d88a28222
Bjorn Nelson <bjornhnelson@gmail.com>
2021-08-30 21:59:51 -0600

    implement finder and writer scripts

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,2 +1,29 @@
-#!/bin/sh
+#!/bin/bash
+# Author: Bjorn Nelson
+
+# save command line arguments
+filesdir=$1
+searchstr=$2
+
+# verify that there are exactly 2 arguments
+if [ $# -ne 2 ]
+then
+	echo "ERROR: invalid number of arguments"
+	exit 1
+fi
+
+# verify that the path is valid
+if [ ! -d "$filesdir" ]
+then
+	echo "ERROR: invalid search path"
+	exit 1
+fi
+
+# run the scripts
+num_files=$(find $filesdir -type f | wc -l)
+num_matches=$(grep -r "$searchstr" $filesdir | wc -l)
+
+# print the results
+echo "The number of files are ${num_files} and the number of matching lines are ${num_matches}"
+exit 0
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,2 +1,46 @@
-#!/bin/sh
+#!/bin/bash
+# Author: Bjorn Nelson
+
+# save command line arguments
+writefile=$1
+writestr=$2
+
+# verify that there are exactly 2 arguments
+if [ $# -ne 2 ]
+then
+	echo "ERROR: invalid number of arguments"
+	exit 1
+fi
+
+# get path without file name
+directory_name=$(dirname $writefile)
+
+# get file name without prepended path
+file_name=$(basename $writefile)
+
+# create directory, if it does not already exist
+if [ ! -d $directory_name ]
+then
+	mkdir -p "$directory_name"
+
+	if [ ! -d $directory_name ]
+	then
+		echo "ERROR: could not create directory"
+		exit 1
+	fi
+fi
+
+# write to file
+cd $directory_name
+echo $writestr > $file_name
+
+# verify that file was written to
+if [ $? -eq 0 ]
+then
+	echo "File written to successfully"
+	exit 0
+else
+	echo "ERROR: file not written to"
+	exit 1
+fi
 
